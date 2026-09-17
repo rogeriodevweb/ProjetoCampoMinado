a78fa25e9a124a2d5616efbdd65489d3652cb3a6
rogeriodevweb <piuutrapp@gmail.com>
2026-09-17 17:54:40 -0300

    Campo minado

---
+++ CampoMinado/src/main/java/com/mycompany/campominado/CampoMinado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.campominado;
+
+/**
+ *
+ * @author RogerioSilva
+ */
+public class CampoMinado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
